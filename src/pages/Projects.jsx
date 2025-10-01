86d53006437f9ad44331389e79a1b17fe0e9bf01
surajpandatwifi <amouraworks@gmail.com>
2025-10-01 17:29:25 +0530

    Updated Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,4 @@
-import { useGSAP } from '@gsap/react'
+/import { useGSAP } from '@gsap/react'
 import VideoGrid from '../components/projects/VideoGrid'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
